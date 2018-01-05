6a2ef5bec546e34395a6df743411fa1622458f42
Vincent La <vincela15@gmail.com>
2018-01-04 20:53:19 -0800

    setting up service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CoinPriceService } from '../services/coinprice';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   providers: [
     StatusBar,
+    CoinPriceService,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
